9b2a96fede0a4dfd7954ca38def02df956276468
Tom Collier <collier@apartmentlist.com>
2018-05-20 19:37:58 -0700

    Add CCW traversal

--- tests/test_spiral_matrix.py
+++ tests/test_spiral_matrix.py
@@ -5,22 +5,32 @@ from spiral.spiral_matrix import SpiralMatrix
 class TestSpiralMatrix(TestCase):
     def test_traverse_empty(self):
         matrix = []
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual([], actual)
 
     def test_traverse_empty_vector(self):
         matrix = [[]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual([], actual)
 
     def test_traverse_single_element(self):
         matrix = [[1]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
+        self.assertEqual([1], actual)
+
+    def test_traverse_single_element_ccw(self):
+        matrix = [[1]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
         self.assertEqual([1], actual)
 
     def test_traverse_row_vector(self):
         matrix = [[1, 2, 3]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
+        self.assertEqual([1, 2, 3], actual)
+
+    def test_traverse_row_vector_ccw(self):
+        matrix = [[1, 2, 3]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
         self.assertEqual([1, 2, 3], actual)
 
     def test_traverse_column_vector(self):
@@ -28,32 +38,64 @@ class TestSpiralMatrix(TestCase):
             [1],
             [2],
             [3]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
+        self.assertEqual([1, 2, 3], actual)
+
+    def test_traverse_column_vector_ccw(self):
+        matrix = [
+            [1],
+            [2],
+            [3]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
         self.assertEqual([1, 2, 3], actual)
 
     def test_traverse_even_square(self):
         matrix = [
             [1, 2],
             [3, 4]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual([1, 2, 4, 3], actual)
+
+    def test_traverse_even_square_ccw(self):
+        matrix = [
+            [1, 2],
+            [3, 4]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
+        self.assertEqual([1, 3, 4, 2], actual)
 
     def test_traverse_odd_square(self):
         matrix = [
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual([1, 2, 3, 6, 9, 8, 7, 4, 5], actual)
+
+    def test_traverse_odd_square_ccw(self):
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
+        self.assertEqual([1, 4, 7, 8, 9, 6, 3, 2, 5], actual)
 
     def test_traverse_wide_rectangle(self):
         matrix = [
             [1, 2, 3, 4],
             [5, 6, 7, 8],
             [9, 10, 11, 12]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual(
             [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7], actual)
+
+    def test_traverse_wide_rectangle_ccw(self):
+        matrix = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
+        self.assertEqual(
+            [1, 5, 9, 10, 11, 12, 8, 4, 3, 2, 6, 7], actual)
 
     def test_traverse_tall_rectangle(self):
         matrix = [
@@ -61,14 +103,24 @@ class TestSpiralMatrix(TestCase):
             [4, 5, 6],
             [7, 8, 9],
             [10, 11, 12]]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual(
             [1, 2, 3, 6, 9, 12, 11, 10, 7, 4, 5, 8], actual)
+
+    def test_traverse_tall_rectangle_ccw(self):
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [10, 11, 12]]
+        actual = [i for i in SpiralMatrix(matrix, clockwise=False)]
+        self.assertEqual(
+            [1, 4, 7, 10, 11, 12, 9, 6, 3, 2, 5, 8], actual)
 
     def test_traverse_large_matrix(self):
         matrix = [[i * 1000 + j for j in range(0, 1000)]
                   for i in range(0, 1000)]
-        actual = [item for item in SpiralMatrix(matrix)]
+        actual = [i for i in SpiralMatrix(matrix)]
         self.assertEqual([0, 1, 2, 3], actual[0:4])
         self.assertEqual([499499, 499500, 500500, 500499],
                          actual[-4:])
