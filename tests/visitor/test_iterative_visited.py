607a945f387cd039d864dbc0d772c2f65ba00c94
Tom Collier <collier@apartmentlist.com>
2018-05-20 17:44:27 -0700

    Add benchmarking

--- tests/visitor/test_iterative_visited.py
+++ tests/visitor/test_iterative_visited.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
 from spiral.visitor.iterative_visitor import IterativeVisitor
+from spiral.traverser import Traverser
 from ..test_traverser import TestTraverser
 
 
 class TestIterativeVisited(TestTraverser.Shared, TestCase):
     def visitor(self):
         return IterativeVisitor(self.accumulator.append)
+
+
+def test_iterative_visitor_large_matrix(benchmark):
+    matrix = [[i * 20 + j for j in range(0, 20)]
+              for i in range(0, 20)]
+    accumulator = []
+    benchmark(Traverser(matrix).start, IterativeVisitor(accumulator.append))
--- tests/visitor/test_recursive_visitor.py
+++ tests/visitor/test_recursive_visitor.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from spiral.visitor.recursive_visitor import RecursiveVisitor
+from spiral.traverser import Traverser
 from ..test_traverser import TestTraverser
 
 
@@ -10,3 +11,10 @@ class TestRecursiveVisitor(TestTraverser.Shared, TestCase):
     # This test is known to fail with a RecursionError
     def test_traverse_large_matrix(self):
         pass
+
+
+def test_recursive_visitor_large_matrix(benchmark):
+    matrix = [[i * 20 + j for j in range(0, 20)]
+              for i in range(0, 20)]
+    accumulator = []
+    benchmark(Traverser(matrix).start, RecursiveVisitor(accumulator.append))
