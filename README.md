0ada2e1490b4b31ac2098de41d5a51bd25485a76
Tom Collier <collier@apartmentlist.com>
2018-05-20 18:25:51 -0700

    Create SpiralMatrix v0.1.0
    Allows clients to iterate over a 2 dimensional matrix in a clockwise
    spiral path.

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 # Spiral Matrix Traversal
 
+Iterate over a 2-D matrix in a spiral path, e.g.
+
+```python
+from spiral.spiral_matrix import SpiralMatrix
+
+matrix = SpiralMatrix([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]])
+
+for item in matrix:
+    print(item)
+# 1
+# 2
+# 3
+# 4
+# 8
+# 12
+# 11
+# 10
+# 9
+# 5
+# 6
+# 7
+```
+
+# Misc
+
 [See research documentation](docs/research.md)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from spiral.version import VERSION
+
+
+def do_setup():
+    setup(
+      name='spiral',
+      packages=['spiral'],
+      version=VERSION,
+      description='Iterate over a 2-D matrix in a spiral manner',
+      author='Tom Collier',
+      author_email='tcollier@gmail.com',
+      url='https://github.com/tcollier/spiral',
+      download_url=(
+        'https://github.com/tcollier/spiral/archive/' + VERSION +
+        '.tar.gz'),
+      keywords=['spiral', 'matrix'],
+      install_requires=[],
+      classifiers=['Programming Language :: Python :: 3']
+    )
+
+
+if __name__ == '__main__':
+    do_setup()
