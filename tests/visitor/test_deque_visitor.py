bd6c7346c19a4c2d43f01796622e869afc82cbcd
Tom Collier <collier@apartmentlist.com>
2018-05-20 17:44:27 -0700

    Add benchmarking

--- tests/visitor/test_deque_visitor.py
+++ tests/visitor/test_deque_visitor.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
 from spiral.visitor.deque_visitor import DequeVisitor
+from spiral.traverser import Traverser
 from ..test_traverser import TestTraverser
 
 
 class TestDequeVisitor(TestTraverser.Shared, TestCase):
     def visitor(self):
         return DequeVisitor(self.accumulator.append)
+
+
+def test_deque_visitor_large_matrix(benchmark):
+    matrix = [[i * 20 + j for j in range(0, 20)]
+              for i in range(0, 20)]
+    accumulator = []
+    benchmark(Traverser(matrix).start, DequeVisitor(accumulator.append))
