4b3e1480ea88d990efd453a0d5ac550ff367dd1b
Tom Collier <collier@apartmentlist.com>
2018-05-21 22:32:44 -0700

    Inline direction and visit tracking

--- spiral/research/visitor/iterative_visitor.py
+++ spiral/research/visitor/iterative_visitor.py
@@ -3,34 +3,41 @@ Visit each element of a matrix in a spiral path using an iterative approach.
 This algorithm keeps track of which elements have already been visited to ensure
 that no element is visited twice.
 """
-from spiral.research.direction import Direction
-from spiral.research.visit_tracker import VisitTracker
 
 
 class IterativeVisitor(object):
-    COMPASS = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+    COMPASS = [
+        (0, 1),   # Right
+        (1, 0),   # Down
+        (0, -1),  # Left
+        (-1, 0)]  # Up
 
     def __init__(self, visit_fn):
         self.__visit_fn = visit_fn
 
     def start(self, matrix, clockwise):
         direction_index = 0 if clockwise else 1
-        i = 0
-        j = 0
-        visit_tracker = VisitTracker(matrix)
+        i, j = (0, 0)
+        visited = [[False for _ in vector] for vector in matrix]
+        height = len(matrix)
+        width = len(matrix[0])
 
-        num_elements = len(matrix) * len(matrix[0])
+        num_elements = height * width
 
         # We know a priori that we want to visit each element in the matrix
         # exactly once, so we can iterate `num_elements` times
         for _ in range(num_elements):
             self.__visit_fn(matrix[i][j])
-            visit_tracker.visit(i, j)
+            visited[i][j] = True
 
-            next_i, next_j = self.COMPASS[direction_index](i, j)
-            if visit_tracker.visitable(next_i, next_j):
-                i, j = next_i, next_j
+            direction = self.COMPASS[direction_index]
+            i2, j2 = i + direction[0], j + direction[1]
+            if 0 <= i2 < height and 0 <= j2 < width and not visited[i2][j2]:
+                i, j = i2, j2
             else:
+                # Turn 90 degrees in the proper direction
                 direction_index = direction_index + (1 if clockwise else -1)
                 direction_index %= 4
-                i, j = self.COMPASS[direction_index](i, j)
+                direction = self.COMPASS[direction_index]
+
+                i, j = i + direction[0], j + direction[1]
